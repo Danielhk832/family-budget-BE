10754e2a2c8b317fad687c79dae25e32a8f3f200
Danielhk832 <aox.soljah@gmail.com>
2022-03-13 08:21:58 +0900

    db migration setup

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -4,7 +4,26 @@ exports.up = async (knex) => {
     tbl.string("username", 200).notNullable();
     tbl.string("password", 200).notNullable();
     tbl.string("email", 200).notNullable();
-  });
+
+  })
+  .createTable("accounts", (tbl) => {
+    tbl.increments("account_id");
+    tbl.integer("balance").notNullable();
+    tbl.string("account_type", 200).notNullable();
+  })
+  .createTable("user_joint_accounts", (tbl) => {
+    tbl.increments("user_joint_account_id")
+    tbl.integer("user_id").notNullable()
+    tbl.integer("account_id").notNullable()
+  })
+  .createTable())
+  .createTable("transactions", (tbl) => {
+    tbl.increments("transaction_id");
+    tbl.string("transaction_type", 200).notNullable();
+    tbl.integer("amount").notNullable();
+    tbl.integer("account_id").notNullable();
+    tbl.integer("budget_id").notNullable();
+  })
 };
 
 exports.down = async (knex) => {
