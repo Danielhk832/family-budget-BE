6a1254b1f43d508ccde7e0f5a2e2cc4c93e60f36
Daniel Kish <aox.soljah@gmail.com>
2022-04-11 02:26:00 -0500

    cleaned up users model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,9 +1,12 @@
 const db = require("../data/db-config.js");
 
 function getAllUsers() {
-  return db("users as u")
-    .join("roles as r", "u.role_id", "r.role_id")
-    .select("u.user_id", "u.username", "u.password", "u.email", "r.role_name");
+  return db("users as u").select(
+    "u.user_id",
+    "u.username",
+    "u.password",
+    "u.email"
+  );
 }
 
 async function insertUser(user) {
@@ -12,28 +15,25 @@ async function insertUser(user) {
   // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
   const [newUserObject] = await db("users").insert(
     {
-      role_id: user.role_id,
       username: user.username,
       email: user.email,
       password: user.password,
       name: user.name,
     },
-    ["username", "password", "email", "role_id"]
+    ["username", "password", "email"]
   );
   return newUserObject; // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
 }
 
 function findBy(filter) {
   return db("users as u")
-    .join("roles as r", "u.role_id", "r.role_id")
     .select("u.user_id", "u.username", "u.password", "r.role_name")
     .where(filter);
 }
 
 function findById(user_id) {
   return db("users as u")
-    .join("roles as r", "u.role_id", "=", "r.role_id")
-    .select("u.user_id", "u.username", "u.email", "r.role_name", "u.name")
+    .select("u.user_id", "u.username", "u.email", "u.name")
     .where("u.user_id", user_id)
     .first();
 }
@@ -46,26 +46,6 @@ async function updateUser(user_id, userDetails) {
 async function deleteUser(user_id) {
   const deletedUser = await db("users").where("user_id", user_id).del();
   return deletedUser;
-}
-
-// SELECT SUM()
-// FROM products
-// WHERE supplierID = 1
-
-// SELECT COUNT(cs.user_id) as times_registered , ct.class_type_name FROM class_signups as cs
-// JOIN classes as cl on cl.class_id = cs.class_id
-// JOIN class_types as ct on cl.class_type_id = ct.class_type_id
-// WHERE cs.user_id = 3
-// GROUP BY ct.class_type_name
-
-async function userPunchCard(user_id) {
-  return await db("class_signups as cs")
-    .count("cs.user_id as times_registered", "ct.class_type_name")
-    .join("classes as cl", "cl.class_id", "cs.class_id")
-    .join("class_types as ct", "cl.class_type_id", "ct.class_type_id")
-    .groupBy("ct.class_type_name")
-    .where("user_id", user_id);
-  // .andWhere("class_id", class_id)
 }
 
 module.exports = {
