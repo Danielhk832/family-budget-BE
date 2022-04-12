f6439934e54bb3bd825c68e7fa04eb0714ed61a2
Danielhk832 <aox.soljah@gmail.com>
2022-04-11 20:17:14 -0500

    auth router setup

---
+++ api/auth/auth-router.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const { JWT_SECRET } = require("../secrets");
+
+const Users = require("../users/users-model");
+const { tokenBuilder } = require("./token-builder");
+const {
+  checkUsernameExists,
+  checkUsernameUnique,
+  validateRole,
+} = require("./auth-middleware");
+
+router.post(
+  "/register",
+  checkUsernameUnique,
+  validateRole,
+  async (req, res, next) => {
+    try {
+      let user = req.body;
+
+      const rounds = process.env.BCRYPT_ROUNDS || 8;
+      const hash = bcrypt.hashSync(user.password, rounds);
+
+      user.password = hash;
+
+      const newUser = await Users.insertUser(user);
+      res.status(201).json(newUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post("/login", checkUsernameExists, (req, res, next) => {
+  if (bcrypt.compareSync(req.body.password, req.user.password)) {
+    const token = tokenBuilder(req.user);
+    res.status(200).json({
+      message: `${req.user.username} is logged in`,
+      token,
+    });
+  } else {
+    next({ status: 401, message: "Invalid credentials" });
+  }
+});
+
+module.exports = router;
